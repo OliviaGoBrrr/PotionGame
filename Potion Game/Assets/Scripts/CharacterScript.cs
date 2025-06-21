cfd4183d650bde8da3b0917b9ade6c662eb990bd
ThePinkOtter <SomePinkOtter@gmail.com>
2025-06-21 17:21:19 +1000

    character scriptttttttt
    zzz

---
+++ Potion Game/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,79 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterScript : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+
+    [Header("Character Sprites")]
+    [SerializeField] Sprite defaultPose;
+    [SerializeField] Sprite happyPose;
+    [SerializeField] Sprite neutralPose;
+    [SerializeField] Sprite unhappyPose;
+
+    [Header("Dialogue Scriptable Objects")]
+    [SerializeField] DialogueScriptableObject introductionDialogue;
+    [SerializeField] DialogueScriptableObject happyDialogue;
+    [SerializeField] DialogueScriptableObject neutralDialogue;
+    [SerializeField] DialogueScriptableObject unhappyDialogue;
+    [SerializeField] List<DialogueScriptableObject> idleDialogue;
+
+    [Header("Potion values")]
+    [Tooltip("The minumum value for the temperature attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int tempFloor;
+    [Tooltip("The maximum value for the temperature attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int tempCeiling;
+    [Tooltip("The minumum value for the carbination attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int carbFloor;
+    [Tooltip("The maximum value for the carbination attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int carbCeiling;
+    [Tooltip("The minumum value for the pazaz attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int pazazFloor;
+    [Tooltip("The maximum value for the pazaz attribute to be 'correct' (0 ~ 10, this value is included)")]
+    [SerializeField] int pazazCeiling;
+    [Tooltip("The minimum value for the potency to be 'correct' (0 ~ 1, this value is included)")]
+    [SerializeField] float potencyFloor;
+    bool active;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void SetActive(bool Active)
+    {
+        active = Active;
+    }
+    void Update()
+    {
+        FadeSet();
+    }
+    private void FadeSet()
+    {
+        if (active == true && spriteRenderer.color.a < 1)
+        {
+            spriteRenderer.color = new Color(0, 0, 0, spriteRenderer.color.a + Time.deltaTime);
+        }
+        else if (active == true && spriteRenderer.color.a > 0)
+        {
+            spriteRenderer.color = new Color(0, 0, 0, spriteRenderer.color.a + Time.deltaTime);
+        }
+    }
+    public void SetSprite(int spriteNum) // sets sprite with ints from 0 to 3
+    {
+        switch(spriteNum)
+        {
+            case 0:
+                spriteRenderer.sprite = defaultPose; 
+                break;
+            case 1:
+                spriteRenderer.sprite = happyPose;
+                break;
+            case 2:
+                spriteRenderer.sprite = neutralPose;
+                break;
+            case 3:
+                spriteRenderer.sprite = unhappyPose;
+                break;
+        }
+    }
+}
